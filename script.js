18a488afaade0987a0742df1b5295f1fc61b1690
devcauanbernardino <devcauanbernardino@gmail.com>
2025-11-03 16:55:06 -0300

    responsive

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+let person = {
+    name: 'Cauan',
+    age: 21,
+    email: 'cauancbd@gmail.com'
+}
+
+let listaCoisas = ['Banana', 'Maça', 'Abacate']
+
+console.log(person)
+console.log(listaCoisas)
+console.log(listaCoisas[1])
+
+listaCoisas.push('Limão')
+listaCoisas.sort()
+console.log(listaCoisas)
+
+let num = 1
+
+while(num <= 10) {
+    console.log(num)
+    num++
+}
+
 
